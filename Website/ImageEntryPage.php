77e3b5d2474407e4418244b40a87483e73c8bd6f
jack <71027978+Jack01110@users.noreply.github.com>
2025-04-01 10:39:06 +0100

    Update ImageEntryPage.php

--- Website/ImageEntryPage.php
+++ Website/ImageEntryPage.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>captcha</title>
     <link rel="stylesheet" type="text/css" href="CSS/stylesheet.css">
 </head>
 
@@ -14,21 +14,28 @@
         require_once "includes/ImagePageScript.inc.php";
     ?>
     <div id="test">
-        <form>
+<<<<<<< HEAD
+        <form id="PictureSelectContainer">
+=======
+        <form action="includes/ImageCheckScript.inc.php" method="post">
+>>>>>>> 8e669a10c965af4fb365c36dad3eae796f0b618e
             <legend>Select the word that matches the image</legend>
             <img src="<?php echo $questionImage?>" width="500" height="500">
+            <div id="RadioButtons2x2">
             <?php foreach ($answers as $answer): ?>
             <label>
-                <input type="radio" name="answer">
+<<<<<<< HEAD
+                <input type="radio" name="answer" id="radio">
+=======
+                <input type="radio" name="answer" value="<?php echo $answer?>">
+>>>>>>> 8e669a10c965af4fb365c36dad3eae796f0b618e
                 <?php echo $answer?>
             </label>
             <?php endforeach ?>
+            </div>
             <button type="submit" name="submit">Submit</button>
         </form>
+        <?php echo 3-$_SESSION['Attempts']?>
     </div>
-
-
-
 </body>
-
 </html>
