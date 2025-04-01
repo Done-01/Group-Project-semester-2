c0a9a629df2b96c24bcf2e6451ef14bd64876ab9
Done-01 <robert-a-dunn@hotmail.com>
2025-04-01 10:59:23 +0100

    fixed it all yo

--- Website/ImageEntryPage.php
+++ Website/ImageEntryPage.php
@@ -14,12 +14,20 @@
         require_once "includes/ImagePageScript.inc.php";
     ?>
     <div id="test">
+<<<<<<< Updated upstream
         <form>
+=======
+        <form action="includes/ImageCheckScript.inc.php" method="post">
+>>>>>>> Stashed changes
             <legend>Select the word that matches the image</legend>
             <img src="<?php echo $questionImage?>" width="500" height="500">
             <?php foreach ($answers as $answer): ?>
             <label>
+<<<<<<< Updated upstream
                 <input type="radio" name="answer">
+=======
+                <input type="radio" name="answer" value="<?php echo $answer?>">
+>>>>>>> Stashed changes
                 <?php echo $answer?>
             </label>
             <?php endforeach ?>
---
+++ Website/includes/CipherScript.inc.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    require_once 'TacFunctions.inc.php';
+
+    $key = $_SESSION['Answer'];
+
+    $string = GenerateRandomString();
+
+    $encriptedString = VigenereEncrypt($string,$key);
+
+    $decryptedString = VigenereDecrypt($encriptedString,$key);
+
+    
+
+
+
+
